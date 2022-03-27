906e1a68411a1e6d914989eb8881cacbd5aa291b
Hannah <hannahwoodward@hotmail.com>
2022-03-27 10:41:18 +0800

    done one error but checked solution and is correct

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,22 @@
 def new_hash
-  # return an empty hash
+  Hash.new
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  {
+    name: "Hannah",
+    age: 33
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  { name: 'Grace Hooper' }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  {id: number}
 end
